9190359f5266de4b9b8f6a8be7c44ed98dbec2c6
Spell <spell@lolwtf.nl>
2020-05-17 14:49:25 +0200

    Custom policy to enable Battle.net OAuth2 sign in

---
+++ src/auth.ts
@@ -0,0 +1,18 @@
+import * as MSAL from "msal";
+
+const msalConfig: MSAL.Configuration = {
+    auth: {
+        clientId: "74bda2a2-4daf-452c-87d7-24def39aab9d",
+        authority: "https://callofblood.b2clogin.com/callofblood.onmicrosoft.com/B2C_1A_BattleNetSignIn",
+        validateAuthority: false,
+        redirectUri: "http://localhost:8080"
+    },
+};
+
+export const auth = new MSAL.UserAgentApplication(msalConfig);
+
+auth.handleRedirectCallback((error, response) => {
+    if (error) {
+        console.log(error);
+    }
+})
--- src/components/Application.tsx
+++ src/components/Application.tsx
@@ -1,22 +1,10 @@
 import * as React from "react";
 import * as MSAL from "msal";
 
-const msalConfig: MSAL.Configuration = {
-    auth: {
-        clientId: "74bda2a2-4daf-452c-87d7-24def39aab9d",
-        redirectUri: "http://localhost:8080"
-    },
-};
-
-const auth = new MSAL.UserAgentApplication(msalConfig);
+import {auth} from "../auth";
 
 export const Application = () => {
-    const [user, setUser] = React.useState<MSAL.Account | null>(null);
-    
-    React.useEffect(() => {
-        setUser(auth.getAccount());
-        console.log(user);
-    });
+    const [user, setUser] = React.useState<MSAL.Account | null>(auth.getAccount());
 
     return (
         <div>
